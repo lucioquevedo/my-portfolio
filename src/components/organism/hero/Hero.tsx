d9b3d6b909ac688a3c22b32128a30c136e737b5f
Lucio Quevedo <85965310+lucioquevedo@users.noreply.github.com>
2024-10-17 20:49:03 +0200

    Text update

--- src/components/organism/hero/Hero.tsx
+++ src/components/organism/hero/Hero.tsx
@@ -46,9 +46,9 @@ const Hero = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 1, duration: 0.4 }}
         >
-          Hello, I&apos;m Lucio, a 20-year-old front-end developer with a
+          Hello, I&apos;m Lucio, a 22-year-old front-end developer with a
           passion for crafting seamless user experiences. I&apos;ve been
-          immersed in the world of web development for the past ~1.5 years,
+          immersed in the world of web development for the past ~2.5 years,
           honing my skills in a dynamic fintech startup environment. While my
           primary focus is front-end development, I also possess a strong
           foundation in back-end development.
